8ef0b9e0f804c10fc801d6ffeef0bbccc646a076
RushiGhorpade <rishighorpade@gmail.com>
2024-06-10 13:15:50 +0530

    2 commit

--- Javaweb/src/main/java/com/example/Javaweb/JavawebApplication.java
+++ Javaweb/src/main/java/com/example/Javaweb/JavawebApplication.java
@@ -1,12 +1,19 @@
 package com.example.Javaweb;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavawebApplication {
+	
+	public static final Logger logger  = LoggerFactory.getLogger(JavawebApplication.class);
 
 	public static void main(String[] args) {
+		
+		logger.info("hello Logger Test");
 		SpringApplication.run(JavawebApplication.class, args);
 		
 		System.out.println("Hii");
