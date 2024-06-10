b4361325fd6252f6278a35c97bcbec18db04584d
RushiGhorpade <rishighorpade@gmail.com>
2024-06-10 12:37:43 +0530

    First Commit

---
+++ Javaweb/src/main/java/com/example/Javaweb/JavawebApplication.java
@@ -0,0 +1,15 @@
+package com.example.Javaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavawebApplication.class, args);
+		
+		System.out.println("Hii");
+	}
+
+}
